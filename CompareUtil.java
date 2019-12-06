6146cb82459cc63100a526791d9df08a3b963d49
Sudip Bolakhe <sudipbolakhe08@gmail.com>
2019-12-06 11:40:04 +0545

    Fix the minor issue.

--- CompareUtil.java
+++ CompareUtil.java
@@ -9,16 +9,11 @@ import java.util.Optional;
 
 public class CompareUtil<T> {
 
-    private final ObjectMapper objectMapper;
-
     private static final Logger LOGGER = LoggerFactory.getLogger(CompareUtil.class);
 
-    public CompareUtil(ObjectMapper objectMapper) {
-        this.objectMapper = objectMapper;
-    }
-
-    public Map<String, String> getUpdatedFieldValue(T original , T updated ){
-        Map<String, String> originalObjectMap =  objectMapper.convertValue(original, Map.class);
+    public Map<String, String> getUpdatedFieldValue(T original, T updated) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map<String, String> originalObjectMap = objectMapper.convertValue(original, Map.class);
         Map<String, String> updatedObjectMap = objectMapper.convertValue(updated, Map.class);
         Map<String, String> updatedValues = getUpdatedValues(originalObjectMap, updatedObjectMap);
         LOGGER.debug("getUpdatedFieldValue() return with updated field-value :{}", updatedValues);
@@ -28,10 +23,12 @@ public class CompareUtil<T> {
     private Map<String, String> getUpdatedValues(Map<String, String> originalObjectMap
             , Map<String, String> updatedObjectMap) {
         Map<String, String> updatedValues = new HashMap<>();
-        for(Map.Entry<String, String> updated : updatedObjectMap.entrySet()){
+        for (Map.Entry updated : updatedObjectMap.entrySet()) {
             Optional<String> key = originalObjectMap.keySet().stream().filter(p -> updated.getKey().equals(p)).findFirst();
-            key.ifPresent(s -> updatedValues.put(s, updated.getValue()));
+            key.ifPresent(s -> {
+                if (updated.getValue() != null && !updated.getValue().equals(originalObjectMap.get(s)))
+                    updatedValues.put(s, String.valueOf(updated.getValue()));
+            });
         }
         return updatedValues;
     }
-}
